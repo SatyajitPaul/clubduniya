608bfd4a3c884fca04a2297add6a20e7a9f7c4d1
Satyajit Paul <paulsjit@gmail.com>
2023-06-11 15:01:40 +0530

    Remove WeSocket

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 const SocketContext = createContext();
 const URL = process.env.REACT_APP_SOCKET_SERVER;
-const socket = io(URL, { transports: ["websocket"] });
+const socket = io(URL);
 
 const ContextProvider = ({ children }) => {
     const [isConnected, setIsConnected] = useState(false);
@@ -12,8 +12,10 @@ const ContextProvider = ({ children }) => {
     //Useing useEffect will initialize the socket coonection
     useEffect(() => {
         socket.on("connect", () => {
+            console.log("Connected");
             setIsConnected(true);
             socket.on("numberOfClients", (data) => {
+                console.log("Count Recived");
                 setClientCount(data.numberOfClients);
             })
         });
